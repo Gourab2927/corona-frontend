a234990c1b2f75910517ee43996f50728e135226
Gourab2927 <63999117+Gourab2927@users.noreply.github.com>
2020-04-21 21:59:21 +0530

    Update BuyMeACoffee.vue

--- components/BuyMeACoffee.vue
+++ components/BuyMeACoffee.vue
@@ -14,7 +14,7 @@
           {
             src: 'https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js',
             'data-name': 'BMC-Widget',
-            'data-id': 'coronatracker',
+            'data-id': '#',
             'data-description': 'Support me on Buy me a coffee!',
             'data-message': 'Thank you for visiting. You can now buy me a coffee!',
             'data-color': '#FF813F',
