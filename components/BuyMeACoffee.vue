1c90d4b92d3c5dd313af860bcbf576f352a4393c
Gourab2927 <63999117+Gourab2927@users.noreply.github.com>
2020-04-21 10:36:17 +0530

    Update BuyMeACoffee.vue

--- components/BuyMeACoffee.vue
+++ components/BuyMeACoffee.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="text-center mt-5">
-    <a class="bmc-button" target="_blank" href="https://www.buymeacoffee.com/coronatracker">
+    <a class="bmc-button" target="_blank" href="https://www.buymeacoffee.com/">
       <img src="https://cdn.buymeacoffee.com/buttons/bmc-new-btn-logo.svg" alt="Buy me a coffee">
       <span style="margin-left:15px;font-size:28px !important;">Buy me a coffee</span>
     </a>
